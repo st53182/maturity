eed1c5f3dfcc3b8c111f3890c441f2997823a361
st53182 <artjoms.grinakins@gmail.com>
2025-06-30 16:32:48 +0300

    MVP 1.0

--- planning_poker.py
+++ planning_poker.py
@@ -9,16 +9,16 @@ planning_bp = Blueprint('planning_poker', __name__)
 def create_room():
     data = request.json
     room = PlanningRoom(name=data['name'])
-    db.add(room)
-    db.commit()
+    db.session.add(room)
+    db.session.commit()
     return jsonify({"room_id": room.id})
 
 @planning_bp.route('/planning-room/<int:room_id>/join', methods=['POST'])
 def join_room(room_id):
     data = request.json
     participant = Participant(name=data['name'], role=data['role'], room_id=room_id)
-    db.add(participant)
-    db.commit()
+    db.session.add(participant)
+    db.session.commit()
     return jsonify({"participant_id": participant.id})
 
 # и так далее: голосование, подсказки, история

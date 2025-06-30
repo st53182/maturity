137c83fb3c9a4ca610119bcc5f2cdb106bee943f
st53182 <artjoms.grinakins@gmail.com>
2025-06-30 15:30:59 +0300

    MVP 1.0

--- planning_poker.py
+++ planning_poker.py
@@ -1,6 +1,6 @@
 # planning_poker.py
 from flask import Blueprint, request, jsonify
-from database import db_session
+from database import db
 from models import PlanningRoom, Participant, Vote
 
 planning_bp = Blueprint('planning_poker', __name__)
@@ -9,16 +9,16 @@ planning_bp = Blueprint('planning_poker', __name__)
 def create_room():
     data = request.json
     room = PlanningRoom(name=data['name'])
-    db_session.add(room)
-    db_session.commit()
+    db.add(room)
+    db.commit()
     return jsonify({"room_id": room.id})
 
 @planning_bp.route('/api/planning-room/<int:room_id>/join', methods=['POST'])
 def join_room(room_id):
     data = request.json
     participant = Participant(name=data['name'], role=data['role'], room_id=room_id)
-    db_session.add(participant)
-    db_session.commit()
+    db.add(participant)
+    db.commit()
     return jsonify({"participant_id": participant.id})
 
 # и так далее: голосование, подсказки, история

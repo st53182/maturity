5ac0096c652e3d7d334f795bddfbc6e78fb91c28
st53182 <artjoms.grinakins@gmail.com>
2025-06-30 15:24:54 +0300

    MVP 1.0

--- socketio_instance.py
+++
@@ -1,3 +0,0 @@
-from flask_socketio import SocketIO
-
-socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
--- wsgi.py
+++
@@ -1,5 +0,0 @@
-from app import app
-from socketio_instance import socketio
-
-if __name__ == "__main__":
-    socketio.run(app)
